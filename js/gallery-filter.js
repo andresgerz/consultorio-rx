44e89d1770ec12dd1f8c7bb9029cf0a3ded61025
Andrés <andres.gerz@gmail.com>
2021-01-12 19:01:58 -0300

    I finished gallery filter

--- js/gallery-filter.js
+++ js/gallery-filter.js
@@ -2,16 +2,18 @@
         //  	itemSelector: '.item',
         //  	layoutMode: 'fitRows'
         //  });
-$('.portfolio-menu ul li').click(function(){
-  $('.portfolio-menu ul li').removeClass('active');
+$('.menu-filter ul li').click(function(){
+  $('.menu-filter ul li').removeClass('active');
   $(this).addClass('active');
   
   var selector = $(this).attr('data-filter');
-  $('.portfolio-item').isotope({
+  
+  $('.gallery-wrapper').isotope({
     filter:selector
   });
   return  false;
 });
+
 $(document).ready(function() {
   var popup_btn = $('.popup-btn');
   popup_btn.magnificPopup({
